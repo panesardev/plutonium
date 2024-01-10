90763542903752e90556e61c91b3bddc49697ff5
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-01-10 08:35:23 -0500

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { inject as injectAnalytics } from '@vercel/analytics';
+
+injectAnalytics({ mode: 'auto' });
 
 bootstrapApplication(AppComponent, appConfig);
