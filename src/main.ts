4307bbe33e771b87f7118987c873982860207ad2
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-01-12 18:38:44 -0500

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { inject as injectAnalytics } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
-
-injectAnalytics({ mode: 'auto' });
-injectSpeedInsights({ debug: true });
 
 bootstrapApplication(AppComponent, appConfig);
