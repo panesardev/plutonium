615bafea2900055407cfda918f733c6f9b7e14b2
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-02-14 15:17:36 -0500

    refactor

--- src/app/utilities/custom.operators.ts
+++ src/app/utilities/custom.operators.ts
@@ -1,21 +1,4 @@
-import { Observable, ObservableInput, combineLatest, isObservable, map, of } from "rxjs";
-
-type SourceObject = {
-  [key: string]: ObservableInput<any>;
-} 
-
-export function view<T>(sourceObject: SourceObject) {
-  const keys = Object.keys(sourceObject);
-  return combineLatest(keys.map(key => sourceObject[key])).pipe(
-    map(array => {
-      const newSourceObject = {};
-      keys.forEach((key, index) => {
-        newSourceObject[key] = array[index];
-      });
-      return newSourceObject;
-    }),
-  ) as Observable<T>;
-}
+import { Observable, combineLatest, isObservable, map, of } from "rxjs";
 
 type InputObject<T> = { [K in keyof T]: T[K] | Observable<T[K]> };
 
