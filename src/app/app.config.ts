6ede1ad4a7619e004acd0468de950d749b6e89e6
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-09-29 09:11:40 -0400

    polishing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideClientHydration } from '@angular/platform-browser';
 import { PreloadAllModules, provideRouter, withComponentInputBinding, withInMemoryScrolling, withPreloading } from '@angular/router';
 import { FIREBASE_CONFIG } from './app.constants';
 import { routes } from './app.routes';
@@ -11,10 +11,10 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideClientHydration(withEventReplay()),
+    provideClientHydration(),
     provideRouter(
       routes,
-      // withPreloading(PreloadAllModules),
+      withPreloading(PreloadAllModules),
       withComponentInputBinding(),
       withInMemoryScrolling({
         scrollPositionRestoration: 'enabled',
