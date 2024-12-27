9c28baf3edbe9a10d944060c14488975e9f8da15
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-12-27 00:45:40 -0500

    refactor

--- src/server/api/routes.ts
+++ src/server/api/routes.ts
@@ -5,7 +5,7 @@ import { readdirSync } from "fs";
 const router = Router();
 
 router.get('/', (request, response) => {
-  const files = readdirSync('./');
+  const files = readdirSync('./dist/plutonium/browser/articles').filter(filename => filename !== 'index.html');
 
   response.json(files);
 });
