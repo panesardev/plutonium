da81be0d6b6a6a74790b9a650cf23d4945350f22
Sukhpreet Singh <panesar.pb08@gmail.com>
2024-12-31 20:13:08 -0500

    refactor

--- src/server/articles/article.util.ts
+++ src/server/articles/article.util.ts
@@ -1,14 +1,14 @@
 import { readdirSync, readFileSync } from "fs";
 
 export function getSlugs() {
-  const isBuildTime = process.env['BUILD_MODE'] === 'true';
-  const files = readdirSync(isBuildTime ? 'src/content/articles' : './dist/plutonium/browser/content/articles');
+  const local = process.env['NODE_ENV'] === 'development' || process.env['BUILD_MODE'] === 'true';
+  const files = readdirSync(local ? 'src/content/articles' : './dist/plutonium/browser/content/articles');
   return files.filter(file => file !== 'index.html');
 }
 
 export function getMarkdown(slug: string) {
-  const isBuildTime = process.env['BUILD_MODE'] === 'true';
-  const path = isBuildTime ? `src/content/articles` : `./dist/plutonium/browser/content/articles`;
+  const local = process.env['NODE_ENV'] === 'development' || process.env['BUILD_MODE'] === 'true';
+  const path = local ? `src/content/articles` : `./dist/plutonium/browser/content/articles`;
   const markdown = readFileSync(`${path}/${slug}/index.md`).toString();
   return markdown;
 }
