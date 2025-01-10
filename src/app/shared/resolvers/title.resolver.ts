e5ff74ffd91f783c0180a73d84c94c960dc4cf42
Sukhpreet Singh <panesar.pb08@gmail.com>
2025-01-10 10:43:20 -0500

    refactor

--- src/app/shared/resolvers/title.resolver.ts
+++ src/app/shared/resolvers/title.resolver.ts
@@ -1,8 +1,9 @@
 import { ActivatedRouteSnapshot, ResolveFn } from "@angular/router";
+import { BRAND } from "@app/app.constants";
 
 export const titleResolver: ResolveFn<string> = (route: ActivatedRouteSnapshot) => {
   const path = route.routeConfig.path;
-  let title = 'Home';
+  let title = BRAND;
 
   if (path) {
     title = path.charAt(0).toUpperCase() + path.slice(1);
